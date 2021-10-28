376673e860d4669f711733ea5f66dafb61877146
JaraCalle <jaracalle2041@gmail.com>
2021-10-28 09:35:44 -0500

    Segundo Commit del proyecto Hackaton 2 - 2021

--- main.py
+++ main.py
@@ -49,76 +49,48 @@ class aplicarFuncionesTriangulo(ttk.Frame):
             "El valor de la hipotenusa es {}".format(round(hipotenusa,2))
             # "¡Hola, {}!".format(self.name_entry.get())
         
-        '''
-            c1 = int(input('valor Cateto Adyacente: '))
-            c2 = int(input('valor Cateto Opuesto: '))
-            valor = math.sqrt((c1*c1) + (c2*c2))
-            print("El valor de la hipotenusa es: ", valor, '\n')
-            print('La formula es: h^2 = raiz(c1^2 + c2^2)\n')
-        '''
+class HallarCateto(ttk.Frame):
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        
+        self.label = ttk.Label(self)
+        self.label2 = ttk.Label(self)
+        self.label3 = ttk.Label(self)
+        self.label["text"] = ("Para hallar un cateto se debe usar el valor de la hipotenusa y el del cateto restante."
+                            "\n\n                                                                   C = h² - C²\n")
+        self.label.pack()
 
-    def ang_int():
-        x = input('Tiene algun angulo interior?: ')
-        if x == 'si' or x=='Si':
-            y = input('Cual es ese angulo?: ')
-            gvalor = 90-y
-            print('El valor del tercer angulo es: ', gvalor)
-        elif x == 'no' or x=='No':
-            y = input('Que lado no tienes?: ')
-            if y == 'ca':
-                print('Se usa la funcion Sen(x) = co/h')
-                co = int(input("Valor cateto opueto: "))
-                h = int(input("Valor hipotenusa: "))
-                gvalor = math.asin(co/h)
-                print('el valor del angulo es: ',math.degrees(gvalor),'°')
-            elif y == 'co':
-                print('Se usa la funcion Cos(x) = ca/h')
-                ca = int(input("Valor cateto adyacente: "))
-                h = int(input("Valor hipotenusa: "))
-                gvalor = math.acos(ca/h)
-                print('el valor del angulo es: ',math.degrees(gvalor),'°')
-            elif y == 'h':
-                print('Se usa la funcion Tan(x) = co/ca')
-                co = int(input("Valor cateto opuesto: "))
-                ca = int(input("Valor cateto adyacente: "))
-                gvalor = math.atan(co/ca)
-                print('el valor del angulo es: ', math.degrees(gvalor),'°')
-            control_ciclo()
-            return gvalor
+        # Cateto opuesto
+        self.label2["text"] = ("Ingrese el valor de la hipotenusa ")
+        self.label2.pack()
 
-'''
-def control_ciclo():
-    cicho = input("Desea ingresar otro dato?\nSi/No ")
-    while cicho=='si'or cicho=='SI' or cicho=='Si' or cicho=='sI':
-        system("cls")
-        Hip_cat()
-    else:
-        system("cls")
-        print('Todo bien PA')
+        self.hipotenusa = ttk.Entry(self)
+        self.hipotenusa.pack()
 
-def Hip_cat():
-    u = input('que necesita hallar? Lados/Angulos: ')
+        # Cateto adyacente
+        self.label3["text"] = ("\nIngrese el valor del cateto ")
+        self.label3.pack()
+        
+        self.cateto = ttk.Entry(self)
+        self.cateto.pack()
 
-    if u == 'lado':
-        x = input('que necesita hallar? Lados/Angulos: ')
-        if x=='h' or x=='H':
-            c1 = int(input('valor Cateto Adyacente: '))
-            c2 = int(input('valor Cateto Opuesto: '))
-            valor = math.sqrt((c1*c1) + (c2*c2))
-            print("El valor de la hipotenusa es: ", valor, '\n')
-            print('La formula es: h^2 = raiz(c1^2 + c2^2)\n')
+        self.greet_button = ttk.Button(
+            self, text="Calcular", command=self.say_hello1)
+        self.greet_button.pack()
+        
+        self.greet_label = ttk.Label(self)
+        self.greet_label.pack()
+    
+    def say_hello1(self):
+        hPt = self.hipotenusa.get()
+        cAd = self.cateto.get()
+        hPt = float(hPt)
+        cAd = float(cAd)
+        cat = math.sqrt((hPt**2)-(cAd**2))
+        self.greet_label["text"] = \
+            "El valor del cateto es {}".format(round(cat,2))
 
-
-        elif x=='c1' or x=='C1' or x=='c2' or x=='C2':
-            h = int(input('valor hipotenusa: '))
-            c2 = int(input('valor Adyacente u Opuesto: '))
-            valor = math.sqrt((h*h) - (c2*c2))
-            print("El valor del cateto es: ", valor, '\n')
-            print('La formula es: c = raiz(h^2 - c^2)\n')
-        control_ciclo()
-    elif u == 'angulo':
-        ang_int()
-'''
 class Explicar(ttk.Frame):
     
     def __init__(self, *args, **kwargs):
@@ -180,6 +152,10 @@ class Application(ttk.Frame):
         self.notebook.add(
             self.aplicarFuncionesTriangulo, text="Hallar la hipotenusa", padding=10)
         
+        #Hallar Cateto
+        self.HallarCateto = HallarCateto(self.notebook)
+        self.notebook.add(
+        self.HallarCateto, text="Hallar un cateto", padding=10)
         
         self.notebook.pack(padx=50, pady=50)
         self.pack()
@@ -187,77 +163,3 @@ class Application(ttk.Frame):
 main_window = tk.Tk()
 app = Application(main_window)
 app.mainloop()
-
-
-'''
-import math
-from os import system
-
-print("Bienvenidos! \nvamos a aprender sobre triangulos rectangulos \nEstas son las partes que componen el triangulo: ")
-print("El triángulo rectángulo es aquel que tiene un ángulo interior que es recto, es decir,\nmide 90º. ... La principal característica del triángulo es que, como ampliaremos más adelante, \ntiene un lado de mayor longitud (llamado hipotenusa) y otros dos denominados catetos cuya \nunión forma el ángulo recto\n")
-print('h = Hipotenusa\nc1 = Cateto1\nc2=cateto2')
-
-def ang_int():
-    x = input('Tiene algun angulo interior?: ')
-    if x == 'si' or x=='Si':
-        y = input('Cual es ese angulo?: ')
-        gvalor = 90-y
-        print('El valor del tercer angulo es: ', gvalor)
-    elif x == 'no' or x=='No':
-        y = input('Que lado no tienes?: ')
-        if y == 'ca':
-            print('Se usa la funcion Sen(x) = co/h')
-            co = int(input("Valor cateto opueto: "))
-            h = int(input("Valor hipotenusa: "))
-            gvalor = math.asin(co/h)
-            print('el valor del angulo es: ',math.degrees(gvalor),'°')
-        elif y == 'co':
-            print('Se usa la funcion Cos(x) = ca/h')
-            ca = int(input("Valor cateto adyacente: "))
-            h = int(input("Valor hipotenusa: "))
-            gvalor = math.acos(ca/h)
-            print('el valor del angulo es: ',math.degrees(gvalor),'°')
-        elif y == 'h':
-            print('Se usa la funcion Tan(x) = co/ca')
-            co = int(input("Valor cateto opuesto: "))
-            ca = int(input("Valor cateto adyacente: "))
-            gvalor = math.atan(co/ca)
-            print('el valor del angulo es: ', math.degrees(gvalor),'°')
-        control_ciclo()
-        return gvalor
-
-
-def control_ciclo():
-    cicho = input("Desea ingresar otro dato?\nSi/No ")
-    while cicho=='si'or cicho=='SI' or cicho=='Si' or cicho=='sI':
-        system("cls")
-        Hip_cat()
-    else:
-        system("cls")
-        print('Todo bien PA')
-
-def Hip_cat():
-    u = input('que necesita hallar? Lados/Angulos: ')
-
-    if u == 'lado':
-        x = input('que necesita hallar? Lados/Angulos: ')
-        if x=='h' or x=='H':
-            c1 = int(input('valor Cateto Adyacente: '))
-            c2 = int(input('valor Cateto Opuesto: '))
-            valor = math.sqrt((c1*c1) + (c2*c2))
-            print("El valor de la hipotenusa es: ", valor, '\n')
-            print('La formula es: h^2 = raiz(c1^2 + c2^2)\n')
-
-
-        elif x=='c1' or x=='C1' or x=='c2' or x=='C2':
-            h = int(input('valor hipotenusa: '))
-            c2 = int(input('valor Adyacente u Opuesto: '))
-            valor = math.sqrt((h*h) - (c2*c2))
-            print("El valor del cateto es: ", valor, '\n')
-            print('La formula es: c = raiz(h^2 - c^2)\n')
-        control_ciclo()
-    elif u == 'angulo':
-        ang_int()
-Hip_cat()
-
-'''
